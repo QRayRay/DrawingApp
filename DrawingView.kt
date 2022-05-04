7fdfb94fa1c253366eee58d7ff8d0a98b3d123c5
Raymond C Quintana <77255338+QRayRay@users.noreply.github.com>
2022-05-03 20:29:12 -0500

    Add files via upload

---
+++ DrawingView.kt
@@ -0,0 +1,15 @@
+package com.example.drawingapp
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+
+
+class DrawingView(context: Context, attrs: AttributeSet) : View(context, attrs) {
+
+
+
+
+
+
+}
